af51bda5a5396446abb94b8c9eb0d3f2a25a3002
thdud <thgml1444@naver.com>
2023-10-24 22:46:27 +0900

    [ ADD ] PWA codebox 기본설정

--- app.mjs
+++ app.mjs
@@ -1,8 +1,15 @@
 export async function Start(){
-  ShowContent('welcome');
+	ShowContent('welcome');
 }
 
 async function ShowContent(ContentName){
   // fill body
   window.document.body.innerHTML = await (await fetch('/contents/' + ContentName + '.html')).text();
+
+  // // do things
+  // switch(ContentName)    {
+  //   case 'config':
+  //     document.getElementById('loginpage_button').addEventListener('click', EL_Click);
+  //     break;
+  // }
 }
